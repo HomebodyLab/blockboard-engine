30a63a633913d7f821409736bd127579cf49be2b
JunSungKang <kjuns1@naver.com>
2024-01-28 01:01:49 +0900

    springboot init code setting.

---
+++ src/test/java/devbox/jpa/step01/Step01ApplicationTests.java
@@ -0,0 +1,13 @@
+package devbox.jpa.step01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Step01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
